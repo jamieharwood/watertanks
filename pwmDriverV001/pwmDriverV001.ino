1de2e6c61ff4203bd5f1bef823ed8c8f078a0270
Jamie <jamieharwood007@gmail.com>
2018-03-17 23:19:14 +0000

    PCB version 0.1

--- pwmDriverV001/pwmDriverV001.ino
+++ pwmDriverV001/pwmDriverV001.ino
@@ -1,3 +1,4 @@
+#include "FastLED.h"
 #include <Servo.h>
 
 Servo waterPumpServo;
@@ -7,46 +8,64 @@ Servo waterPumpServo;
 #define speedControlMax 2000
 
 
-#define pumpPin 5    // Output
-#define minusPin1 7  // Output
-#define inPin0 8     // Input
-#define minusPin2 9  // Output
-#define pwmLedPin0 10     // Input
-#define minusPin3 11  // Output
+#define pumpPin 4    // Output
+//#define minusPin1 7  // Output
+#define inPin0 5     // Input
+//#define minusPin2 9  // Output
+//#define pwmLedPin0 10     // Input
+//#define minusPin3 11  // Output
 
 long pumpPWM = speedControlMid;
 
-void setup() {
-  // initialize both serial ports:
+#define NUM_LEDS 4
+#define DATA_PIN 3
 
+// Define the array of leds
+CRGB leds[NUM_LEDS];
+
+
+void setup() {
+  // initialize serial port
   //Serial.begin(115200);
 
   // Setup the PWM for the pump speed controller
   waterPumpServo.attach(pumpPin);
   waterPumpServo.writeMicroseconds(speedControlMid);
   
-  pinMode(minusPin1, OUTPUT);
-  pinMode(minusPin2, OUTPUT);
-  pinMode(minusPin3, OUTPUT);
-  pinMode(pwmLedPin0, OUTPUT);
+  //pinMode(minusPin1, OUTPUT);
+  //pinMode(minusPin2, OUTPUT);
+  //pinMode(minusPin3, OUTPUT);
+  //pinMode(pwmLedPin0, OUTPUT);
   
-  digitalWrite(minusPin1, LOW);
-  digitalWrite(minusPin2, LOW);
-  digitalWrite(minusPin3, LOW);
-  digitalWrite(pwmLedPin0, LOW);
+  //digitalWrite(minusPin1, LOW);
+  //digitalWrite(minusPin2, LOW);
+  //digitalWrite(minusPin3, LOW);
+  //digitalWrite(pwmLedPin0, LOW);
   
   pinMode(inPin0, INPUT_PULLUP);
+  
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 }
 
 void loop() {  
   if (digitalRead(inPin0) == false) {
     pumpPWM = speedControlMax;
-    digitalWrite(pwmLedPin0, HIGH);
+    //digitalWrite(pwmLedPin0, HIGH);
+    
+    // Turn the LED on, then pause
+    leds[0] = CRGB::Red;
+    FastLED.show();
+    
     //Serial.println("HIGH");
   }
   else {
     pumpPWM = speedControlMid;
-    digitalWrite(pwmLedPin0, LOW);
+    //digitalWrite(pwmLedPin0, LOW);
+    
+    // Now turn the LED off, then pause
+    leds[0] = CRGB::Green;
+    FastLED.show();
+  
     //Serial.println("LOW");
   }
 
